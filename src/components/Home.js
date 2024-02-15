c82300e73bfc3b1aa8a598681b61ea38496496c5
dinesdayrit <dayritdines22@gmail.com>
2024-02-15 16:53:31 +0800

    add LI and Github

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,25 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
 export default function Home() {
-    return (
-      <div className="w-[80rem] mt-16">
-        <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl text-sky-700">
-          Hello, I'm Dines Dayrit! 🌟
-        </h2>
-        <p className="mt-4 text-lg md:text-xl text-gray-800 ml-4">
-          A passionate Web Developer with expertise in React.js and Node.js, I thrive on bringing innovative digital solutions to life. Currently, I am engaged with a leading fintech company here in the Philippines, where I am actively involved in the development of a cutting-edge GIS web application tailored for a Local Government Unit (LGU).
-        </p>
+  return (
+    <div className="w-[80rem] mt-16">
+      <h2 className="font-bold text-3xl md:text-4xl lg:text-5xl text-sky-700">
+        Hello, I'm Dines Dayrit! 🌟
+      </h2>
+      <p className="mt-4 text-lg md:text-xl text-gray-800 ml-4">
+        A passionate Web Developer with expertise in React.js and Node.js, I thrive on bringing innovative digital solutions to life. Currently, I am engaged with a leading fintech company here in the Philippines, where I am actively involved in the development of a cutting-edge GIS web application tailored for a Local Government Unit (LGU).
+      </p>
+
+      <div className="mt-8">
+        <a href="https://www.linkedin.com/in/dines-dayrit-067103284/" target="_blank" rel="noopener noreferrer" className="inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mr-4">
+          <FaLinkedin className="inline-block mr-2" />
+          LinkedIn
+        </a>
+        <a href="https://github.com/dinesdayrit" target="_blank" rel="noopener noreferrer" className="inline-block bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full">
+          <FaGithub className="inline-block mr-2" />
+          GitHub
+        </a>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,4 @@
-import { useState } from "react";
-
 export default function Sidebar(props) {
-  const [showHome, setShowHome] = useState(false);
-
-
-
     const buttonStyles = "px-4 py-4 text-xs md:text-base text-stone-400 hover:text-stone-50 uppercase hover:bg-stone-800";
     
     const homeClick = () =>{
