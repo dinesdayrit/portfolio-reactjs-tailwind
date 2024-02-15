552e4fa8f88de91ebfd878a4c777f276f204c9dd
dinesdayrit <dayritdines22@gmail.com>
2024-02-15 17:34:09 +0800

    update job experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,10 +12,12 @@ export default function Experience() {
         </ul>
 
         <h2 className="font-serif font-bold text-3xl mb-5  text-sky-700 mt-20">Non-Programming Job Experience</h2>
-  
-        <p className="font-serif font-semibold mb-1 text-sky-500">(Apr 2014 - Apr 2017) Sixeleven Global Services - Senior Technical Support</p>
-        <p className="font-serif font-semibold mb-1 text-sky-500">(Aug 2017 - Sept 2020) Alps Maintaineering Services - Asst. Branch Manager</p>
+
         <p className="font-serif font-semibold mb-1 text-sky-500">(Sept 2020 - Aug 2023) Novally - Lead Generation Quality Assurance Specialist</p>
+        <p className="font-serif font-semibold mb-1 text-sky-500">(Aug 2017 - Sept 2020) Alps Maintaineering Services - Asst. Branch Manager</p>
+        <p className="font-serif font-semibold mb-1 text-sky-500">(Apr 2014 - Apr 2017) Sixeleven Global Services - Senior Technical Support</p>
+        
+        
 
 
       </div>
