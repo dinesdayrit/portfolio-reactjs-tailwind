afa3b123106212f088904da614ff066f7a63a39b
dinesdayrit <dayritdines22@gmail.com>
2024-03-18 15:43:20 +0800

    change burger color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,7 +46,7 @@ export default function Navbar() {
          onClick={toggleMenu} 
          data-collapse-toggle="navbar-default" 
          type="button" 
-         className="flex flex-col items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
+         className="flex flex-col items-center p-2 w-10 h-10 justify-center text-sm text-gray-300 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
          aria-controls="navbar-default" 
          aria-expanded={isMenuOpen ? "true" : "false"}
          >
