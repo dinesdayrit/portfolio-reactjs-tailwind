c261649c4ea11201a6dc45815b5e08fb657364c1
dinesdayrit <dayritdines22@gmail.com>
2024-04-02 21:29:35 +0800

    adjust radius on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@
 export default function Contact() {
 
     return (
-      <div className="flex flex-col justify-center items-center  w-auto h-auto bg-blue-500 p-16 rounded-t-full" id='contact'>
+      <div className="flex flex-col justify-center items-center  w-auto h-auto bg-blue-500 p-16 rounded-t-3xl md:rounded-t-full" id='contact'>
         <h2 className="font-serif font-bold text-3xl mb-5  text-orange-500">Contact me</h2>
   
         <p className="font-serif font-semibold mb-1 text-xl text-gray-100 "><i className="fa-brands fa-facebook-messenger mr-1 animate-pulse"></i>Messenger</p>
