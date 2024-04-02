1338ef7c916aaac0068e4164a945e99ef19d2ddf
dinesdayrit <dayritdines22@gmail.com>
2024-04-02 21:40:32 +0800

    adjust text style on About

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About () {
             <img src='./line.svg' width={180}  alt="line" className="" />
             <div className='flex flex-col items-center md:flex-row gap-8 justify-center'>
             <img src='./profilepic.svg' width={250}  alt="profile" className="bg-sky-800 rounded-s-full rounded-tr-xl mt-12" />
-            <p className='md:mt-16 md:w-1/4 font-mono text-xl'>"Hey, I'm Dines Dayrit, a dedicated web developer with a specialization in React.js. I'm all about crafting dynamic and interactive user interfaces that breathe life into web applications."</p>
+            <p className='md:mt-16 ml-2 mr-2 md:w-1/3 font-mono text-xl text-balance text-center md:text-justify'>"Hey, I'm Dines Dayrit, a dedicated web developer with a specialization in React.js. I'm all about crafting dynamic and interactive user interfaces that breathe life into web applications."</p>
             </div>
             
         </section>
